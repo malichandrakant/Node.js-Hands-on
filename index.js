18669bab44ef08b3cc09739d3d5dcd9f98f30d7d
Mali Chandrakant <161549812+malichandrakant@users.noreply.github.com>
2026-02-01 15:23:24 +0530

    Add figlet text output

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello, World!");
+const figlet = require('figlet');
+
+figlet("Hi , I am chandrakant , I love Node.js !!", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
