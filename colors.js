95066ffa3fba451aedbb8c2610c5c932b16db2aa
Mali Chandrakant <161549812+malichandrakant@users.noreply.github.com>
2026-02-03 16:53:38 +0530

    display color package

---
+++ colors.js
@@ -0,0 +1,16 @@
+const colors=require('colors');
+
+console.log(colors.green('This text is green'));
+console.log(colors.red("This text is red"));
+console.log(colors.blue("This text is blue"));
+console.log(colors.yellow("This text is yellow"));
+console.log(colors.magenta("This text is magenta"));
+console.log(colors.cyan("This text is cyan"));
+console.log(colors.white("This text is white"));
+console.log(colors.gray("This text is gray"));
+console.log(colors.black("This text is black"));
+console.log(colors.rainbow("This text has a rainbow effect"));
+console.log(colors.zebra("This text has a zebra effect - My name is Chandrakant"));
+console.log(colors.random("This text has a random color"));
+console.log(colors.bold("This text is bold"));
+console.log(colors.italic("This text is italic"));
