eb06c5e98370dd691019ea0b728dad698ad4286a
Mali Chandrakant <161549812+malichandrakant@users.noreply.github.com>
2026-02-03 13:10:18 +0530

    Add fundamentals of Node.js and core module or global object

---
+++ basics.js
@@ -0,0 +1,60 @@
+
+
+const {userName}=require('./data.js')
+
+console.log(userName);
+
+
+// var a=10;
+// let b=20;
+
+// console.log(a+b);
+
+
+// if(a==20){
+//     console.log("a is 20");
+// }else{
+//     console.log("a is not 20");
+// }
+
+
+// function fruit(item){
+//     console.log("this is"+item);
+// }
+
+// console.log("banana");
+
+
+
+// for(let i=1;i<5;i++){
+//     console.log(i);
+// }
+
+// var a=0;
+// while(a<=10){
+//     a++;
+//     console.log(a);
+// }
+
+// var users=["chandrakant","abhay","Ujwal","Yogesh"];
+
+// for(let i=0;i<users.length;i++){
+//     console.log(users[i]);
+// }
+
+
+// var user={
+//     name:"chandrakant",
+//     age:24,
+//     city:"pune"
+// }
+
+// console.log(user);
+// console.log(user.name);
+// console.log(user.age);
+// console.log(user.city);
+
+// user.age=22;
+// user.city="mumbai";
+// console.log(user);
+// console.log(user.city);
---
+++ module.js
@@ -0,0 +1,3 @@
+const fs=require('fs');
+
+fs.writeFileSync('message.txt','Hello, Node.js!');
